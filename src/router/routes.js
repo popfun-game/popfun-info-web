4bb9fdff040c33bd4176a9b5c88421bab9f4ac02
alpha.wu <alpha.wu@lunarax.io>
2022-01-19 18:57:34 +0800

    chore: 修改构建

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,35 +1,35 @@
 const routes = [
     {
         path: '/',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/home/index'),
+        component: () => import('@/views/home/index'),
     },
     // 资讯
     {
         path: '/information/',
-        component: () => import(/* webpackChunkName: "info" */ '@/views/info/index'),
+        component: () => import('@/views/info/index'),
     },
     // 战略
     {
         path: '/strategy/',
-        component: () => import(/* webpackChunkName: "strategy" */ '@/views/strategy/index'),
+        component: () => import('@/views/strategy/index'),
     },
     // 学习
     {
         path: '/learn/',
-        component: () => import(/* webpackChunkName: "learn" */ '@/views/learn/index'),
+        component: () => import('@/views/learn/index'),
     },
 
     {
         path: '/about-us/',
-        component: () => import(/* webpackChunkName: "about-us" */ '@/views/about-us/index'),
+        component: () => import('@/views/about-us/index'),
     },
     {
         path: '/currency/:coin',
-        component: () => import(/* webpackChunkName: "currency" */ '@/views/detail/currency'),
+        component: () => import('@/views/detail/currency'),
     },
     {
         path: '/currency/:coin/:type',
-        component: () => import(/* webpackChunkName: "currency" */ '@/views/detail/currency'),
+        component: () => import('@/views/detail/currency'),
     },
     {
         path: '/:404*',
